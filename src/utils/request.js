c1ffc69ffac6b68c356f502fd89650ae9c497022
4pril <zzb4pri1@gmail.com>
2025-04-20 20:44:22 +0900

    0.0.5

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const baseURL = "https://35.75.52.34:8080/cards/api";
+const baseURL = "http://35.75.52.34:8080/cards/api";
 const instance = axios.create({baseURL});
 
 instance.interceptors.response.use(
